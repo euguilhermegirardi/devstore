962cca81802daed67fe551fdf25146816b13d2c5
euguilhermegirardi <girardi.gui@icloud.com>
2024-03-11 15:40:13 -0300

    create search-form and tidy-up

--- src/app/(store)/search/loading.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function SearchLoading() {
-    return <h1>Search is loading...</h1>;
-}
-exports.default = SearchLoading;
