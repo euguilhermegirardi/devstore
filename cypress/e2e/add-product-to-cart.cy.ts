e805e0b8917f6aca50fe4b9b9085010849fde93a
euguilhermegirardi <girardi.gui@icloud.com>
2024-03-11 17:14:34 -0300

    create e2e tests and add next config

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,7 +1,10 @@
 describe('add product to cart', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
   it('should be able to navigate to the product page and add a product to cart', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('a[href^="/product"').first().click()
+    cy.get('a[href^="/product"]').first().click()
     cy.url().should('include', '/product')
     cy.location('pathname').should('include', '/product')
     cy.contains('Add to cart').click()
@@ -9,22 +12,10 @@ describe('add product to cart', () => {
   })
 
   it('should not count duplicated products in the cart', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('a[href^="/product"').first().click()
+    cy.get('a[href^="/product"]').first().click()
     cy.url().should('include', '/product')
     cy.location('pathname').should('include', '/product')
     cy.contains('Add to cart').click()
-    cy.contains('Add to cart').click()
-    cy.contains('Cart (1)').should('exist')
-  })
-})
-
-describe('search product', () => {
-  it('should be able to search for a product and add it to the cart', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
-    cy.get('a[href^="/product"').first().click()
-    cy.location('pathname').should('include', '/product')
     cy.contains('Add to cart').click()
     cy.contains('Cart (1)').should('exist')
   })
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['github.com'],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 }
 
