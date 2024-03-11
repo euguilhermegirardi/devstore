cd9886244b183cad0619369d84e5dd3c4a949f05
euguilhermegirardi <girardi.gui@icloud.com>
2024-03-11 16:00:24 -0300

    create search loading

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,25 @@
+'use client'
+import Skeleton from '@/components/skeleton'
+import { useSearchParams } from 'next/navigation'
+
 export default function SearchLoading() {
-  return <h1>Search is loading...</h1>
+  const searchParams = useSearchParams()
+  const query = searchParams.get('q')
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Search for: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[395px]" />
+        <Skeleton className="h-[395px]" />
+        <Skeleton className="h-[395px]" />
+        <Skeleton className="h-[395px]" />
+        <Skeleton className="h-[395px]" />
+        <Skeleton className="h-[395px]" />
+      </div>
+    </div>
+  )
 }
